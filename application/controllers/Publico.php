74b741ae6ea0640b1e4a9139867f8764610bf14f
hendrydasilva <dasilva.hendry@gmail.com>
2017-05-23 18:57:02 +0100

    resolviendo index publico

--- application/controllers/Publico.php
+++ application/controllers/Publico.php
@@ -16,34 +16,7 @@ class Publico extends CI_Controller {
 		// var_dump($this->Automovel_model->obterTodosAutomoveis());
 		$search = array('fabricante' => "Toyota");
 		// var_dump($this->Automovel_model->obterAutomoveisPorFiltro($search));
-		var_dump($this->Automovel_model->obterListaMatriculas());
-
-		$this->load->library('pagination');
-		$form_url = "publico/index";
-
-		if (count($search) > 0) {
-			$form_url .= '?'.http_build_query($search,'',"&");
-		}
-
-		$offset = $this->input->get("page")??0;
-
-		$config['base_url'] = base_url($form_url);//redefinido para a paginação
-		$config['enable_query_strings']= TRUE;
-		$config['page_query_string']= true;
-		
-		$config['total_rows'] = $this->Automovel_model->obterAutomoveisPorFiltro($search);
-		$this->pagination->initialize($config);
-		$config['per_page'] = ITEMS_PER_PAGE;
-
-		$data['title']= $this->input->get('title');
-		$data['author']= $this->input->get('author');
-		$data['author']= $this->input->get('author');
-        
-
-		$data['search_results_count'] = $config['total_rows'];
-		$data['search_pagination'] = $this->pagination->create_links();
-		$data['search_results'] = $this->Automovel_model->obterAutomoveisPorFiltro($search, $offset);
-
+		//var_dump($this->Automovel_model->obterListaMatriculas());
 		$data['active_menu'] = 'home';
 		$data['content']     = 'home';
 		$this->load->view('init',$data);
