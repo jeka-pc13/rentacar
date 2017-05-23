f1dcc51e3e9e5d38ceb1382ba56c92960c6c28e8
canutolab <canutolab@gmail.com>
2017-05-23 11:15:15 +0100

    CommitTest
    teste

--- application/models/Automovel_model.php
+++ application/models/Automovel_model.php
@@ -1,7 +1,7 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Automovel_model extends CI_Model {
+class Automovel_model extends CI_Model { 
 
 	public function __construct(){
 		parent::__construct();
@@ -14,25 +14,19 @@ class Automovel_model extends CI_Model {
 
 	public function obterTodosAutomoveis(){
 
-
 			$select = "autos.id as id,
 			m.nome as modelo,
 			f.nome as fabricante,
 			c.nome as cor,
-			autos.disponibilidade as disponibilidade
-			";
-
+			autos.disponibilidade as disponibilidade";
 
 			$this->db->select($select)
 			->from("automoveis.automoveis autos")
-
 			->join("automoveis.cores c", "autos.cor_id = c.id") //Cores
 			->join("automoveis.modelos m", "autos.modelo_id = m.id")//modelo 
 			->join("automoveis.fabricantes f", "m.fabricante-id = f.id")
 			->group_by("autos.id");
 			// ->limit($limit,$offset);
-
-
 			return $this->db->get()->result();
 	}
 }
