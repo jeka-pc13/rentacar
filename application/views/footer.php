113c5c7d9fb518fd0c1379747bd64e71ab5d0905
hendrydasilva <dasilva.hendry@gmail.com>
2017-05-23 18:54:39 +0100

    salvando el footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,27 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div class="footer text-center">
-<p>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+  <p>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
+<!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
   $( function() {
     $( "#data_publicacao" ).datepicker(
     {
-        dateFormat: 'yy-mm-dd'
+      dateFormat: 'yy-mm-dd'
     }
-);
+    );
   } );
-  </script>
+</script>
 
-  <!-- Custom JS files -->
-  <?php if($js_files ?? false){
-        foreach ($js_files as $js_file): ?>
+<!-- Custom JS files -->
+<?php if($js_files ?? false):?>
+  <?php    foreach ($js_files as $js_file): ?>
     <link href="<?php echo base_url('/assets/js/'.$js_file)?>" rel="stylesheet">
-    <?php endforeach; ?>
+  <?php endforeach; ?>
+<?php endif; ?>
+
 
 
