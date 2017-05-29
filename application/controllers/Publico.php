e1a99c7e2988f108094b88b00fa499edd6ddc9d2
jeka-pc13 <2054710@student.uma.pt>
2017-05-29 09:20:50 +0100

    Publico e contacto
    enviar email

--- application/controllers/Publico.php
+++ application/controllers/Publico.php
@@ -20,6 +20,11 @@ class Publico extends CI_Controller {
 
 			// var_dump($this->db);
 			// var_dump($this->db->conn_id)
+			$this->load->helper('form');
+$this->load->library('form_validation');
+$this->load->library('encrypt');
+$this->load->helper('security');
+
 	}
 	
 	public function index(){
@@ -55,7 +60,63 @@ class Publico extends CI_Controller {
 		//var_dump($this->Automovel_model->obterAutomoveisPorFiltro($search));
 		//var_dump($this->Automovel_model->obterTodosAutomoveis());
 		$data['active_menu'] = 'contact';
-		$data['content']     = 'contacto';
+		$data['content']     = 'contacto_prueba';
 		$this->load->view('init',$data);
+	}
+
+	// Send Gmail to another user
+	public function Send_Mail() {
+
+// Check for validation
+		$this->form_validation->set_rules('user_email', 'User Email', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('user_password', 'User Password', 'trim|required');//|xss_clean');
+		$this->form_validation->set_rules('to_email', 'To', 'trim|required');//|xss_clean');
+		$this->form_validation->set_rules('subject', 'Subject', 'trim|required');//|xss_clean');
+		$this->form_validation->set_rules('message', 'Message', 'trim|required');//|xss_clean');
+		if ($this->form_validation->run() == FALSE) {
+			//$this->load->view('view_form');
+			$data['active_menu'] = 'contact';
+		$data['content']     = 'contacto_prueba';
+		$this->load->view('init',$data);
+		} else {
+
+// Storing submitted values
+			$sender_email = $this->input->post('user_email');
+			$user_password = $this->input->post('user_password');
+			$receiver_email = $this->input->post('to_email');
+			$username = $this->input->post('name');
+			$subject = $this->input->post('subject');
+			$message = $this->input->post('message');
+
+// Configure email library
+			$config['protocol'] = 'smtp';
+			$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+			$config['smtp_port'] = 465;
+			$config['smtp_user'] = $sender_email;
+			$config['smtp_pass'] = $user_password;
+
+// Load email library and passing configured values to email library
+			$this->load->library('email', $config);
+			$this->email->set_newline("\r\n");
+
+// Sender email address
+			$this->email->from($sender_email, $username);
+// Receiver email address
+			$this->email->to($receiver_email);
+// Subject of email
+			$this->email->subject($subject);
+// Message in email
+			$this->email->message($message);
+
+			if ($this->email->send()) {
+				$data['message_display'] = 'Email Successfully Send !';
+			} else {
+				$data['message_display'] =  '<p class="error_msg">Invalid Gmail Account or Password !</p>';
+			}
+			//$this->load->view('publico/contacto', $data);
+			$data['active_menu'] = 'contact';
+		$data['content']     = 'contacto_prueba';
+		$this->load->view('init',$data);
+		}
 	}
 }
---
+++ application/views/contacto_prueba.php
@@ -0,0 +1,84 @@
+
+<body>
+	<div class="main">
+		<div id="content">
+			<h2 id="form_head">Codelgniter Email</h2>
+			<div id="form_input">
+				<div class="msg">
+					<?php
+					if (isset($message_display)) {
+						echo $message_display;
+					}
+					?>
+				</div>
+				<?php
+				echo '<div class="error_msg">';
+				echo validation_errors();
+				echo "</div>";
+				echo form_open('publico/send_mail');
+				echo form_label('Email-ID');
+				echo "<div class='all_input'>";
+				$data_email = array(
+					'type' => 'email',
+					'name' => 'user_email',
+					'id' => 'e_email_id',
+					'class' => 'input_box',
+					'placeholder' => 'Please Enter Email'
+					);
+				echo form_input($data_email);
+				echo "</div>";
+				echo form_label('Password');
+				echo "<div class='all_input'>";
+				$data_password = array(
+					'name' => 'user_password',
+					'id' => 'password_id',
+					'class' => 'input_box',
+					'placeholder' => 'Please Enter Password'
+					);
+				echo form_password($data_password);
+				echo "</div>";
+				echo form_label('Name');
+				echo "<div class='all_input'>";
+				$data_email = array(
+					'name' => 'name',
+					'class' => 'input_box',
+					'placeholder' => 'Please Enter Name'
+					);
+				echo form_input($data_email);
+				echo "</div>";
+				echo form_label('To');
+				echo "<div class='all_input'>";
+				$data_email = array(
+					'type' => 'email',
+					'name' => 'to_email',
+					'class' => 'input_box',
+					'placeholder' => 'Please Enter Email'
+					);
+				echo form_input($data_email);
+				echo "</div>";
+				echo form_label('Subject');
+				echo "<div class='all_input'>";
+				$data_subject = array(
+					'name' => 'subject',
+					'class' => 'input_box',
+					);
+				echo form_input($data_subject);
+				echo "</div>";
+				echo form_label('Message');
+				echo "<div class='all_input'>";
+				$data_message = array(
+					'name' => 'message',
+					'rows' => 5,
+					'cols' => 32
+					);
+				echo form_textarea($data_message);
+				echo "</div>";
+				?>
+			</div>
+			<div id="form_button">
+				<?php echo form_submit('submit', 'Send', "class='submit'"); ?>
+			</div>
+			<?php echo form_close(); ?>
+		</div>
+	</div>
+</body>
