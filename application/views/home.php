79bc8e3b7f5b9f855e5574689d5285f6af4363f4
hendrydasilva <dasilva.hendry@gmail.com>
2017-05-30 11:14:31 +0100

    Home e Sobre
    Justificación texto párrafos

--- application/views/home.php
+++ application/views/home.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="container">
 		<h1>Vale Rent a Car</h1>
 		<p align="justify">Vale Rent a Car ofereçe viaturas de qualidade e seguras através de um serviço personalizado prestado por staff experiente. Pretendemos ser a sua melhor opção para que possa desfrutar ao máximo a sua estadia na Ilha da Madeira. Pode contar com Quilómetros Ilimitados, possibilidade de aluguer de GPS, seguro extra SCDW para Total Protecção e Isenção de Franquia, possibilidade de aluguer Sem Cartão de Crédito e Cadeiras de Criança seguras.</p>
-		<p>Somos uma empresa reconhecida pelo programa "Fast Lane", que permitiu aumentar a presença no mercado e abrir caminho para a transição de uma empresa tradicional de aluguer de veículos para um operador de serviços de mobilidade e melhorar sua excelência operacional. Este programa de transformação em curso lançou as bases para um crescimento sustentável e rentável.</p>
+		<p align="justify">Somos uma empresa reconhecida pelo programa "Fast Lane", que permitiu aumentar a presença no mercado e abrir caminho para a transição de uma empresa tradicional de aluguer de veículos para um operador de serviços de mobilidade e melhorar sua excelência operacional. Este programa de transformação em curso lançou as bases para um crescimento sustentável e rentável.</p>
 
 		<div class="inner cover">
 			<div id="myCarousel" class="carousel slide" data-ride="carousel">
--- application/views/sobre.php
+++ application/views/sobre.php
@@ -8,9 +8,9 @@
 					<h3 class="panel-title">Missão</h3>
 				</div>
 				<div class="panel-body">
-					<p>É nossa missão adotar estratégias de crescimento e consolidação dos princípios empresariais de proximidade com o Cliente sob o lema de “Alugamos soluções”, mas com a cultura interna de serviço sob a referência de “Qualidade, Rigor e Transparência”.</p>
-					<p>Reconhecida como a maior empresa de aluguer de automóveis da RAM e uma das maiores a nível nacional, tem primado ao longo dos seus mais de 20 anos de existência por manter um serviço com padrões de excelência quer no atendimento quer na qualidade das viaturas que disponibiliza aos seus clientes. Prova deste facto foi a atribuição recente a esta empresa do Certificado do Sistema de Qualidade de acordo com a Norma ISSO 9001:2008.</p>
-					<p>Vale Rent a Car tem ainda a representação exclusiva da AVIS e EUROPCAR, na RAM, criando assim uma serie de vantagens para os clientes que habitualmente optam por estes serviços.</p>
+					<p align="justify">É nossa missão adotar estratégias de crescimento e consolidação dos princípios empresariais de proximidade com o Cliente sob o lema de “Alugamos soluções”, mas com a cultura interna de serviço sob a referência de “Qualidade, Rigor e Transparência”.</p>
+					<p align="justify">Reconhecida como a maior empresa de aluguer de automóveis da RAM e uma das maiores a nível nacional, tem primado ao longo dos seus mais de 20 anos de existência por manter um serviço com padrões de excelência quer no atendimento quer na qualidade das viaturas que disponibiliza aos seus clientes. Prova deste facto foi a atribuição recente a esta empresa do Certificado do Sistema de Qualidade de acordo com a Norma ISSO 9001:2008.</p>
+					<p align="justify">Vale Rent a Car tem ainda a representação exclusiva da AVIS e EUROPCAR, na RAM, criando assim uma serie de vantagens para os clientes que habitualmente optam por estes serviços.</p>
 				</div>
 			</div>
 		</div>
@@ -21,13 +21,13 @@
 					<h3 class="panel-title">Objectivos</h3>
 				</div>
 				<div class="panel-body">
-					<p>- Satisfação total dos Clientes.</p>
-					<p>- Rentabilidade do negócio.</p>
-					<p>- Criação de valor.</p>
-					<p>- Bom relacionamento com fornecedores e parceiros.</p>
-					<p>- Melhoria contínua da eficácia do sistema de gestão.</p>
-					<p>- Motivação e satisfação dos colaboradores.</p>
-					<p>- Cumprimento dos requisitos</p>
+					<p align="justify">- Satisfação total dos Clientes.</p>
+					<p align="justify">- Rentabilidade do negócio.</p>
+					<p align="justify">- Criação de valor.</p>
+					<p align="justify">- Bom relacionamento com fornecedores e parceiros.</p>
+					<p align="justify">- Melhoria contínua da eficácia do sistema de gestão.</p>
+					<p align="justify">- Motivação e satisfação dos colaboradores.</p>
+					<p align="justify">- Cumprimento dos requisitos</p>
 				</div>
 			</div>
 		</div>
@@ -41,10 +41,10 @@
 					<h3 class="panel-title">Datas</h3>
 				</div>
 				<div class="panel-body">
-					<p>2000 - Vale Rent a Car lança um novo serviço de reserva on-line. Este é um grande passo em frente para a empresa. Seu site www.valerentacar.com, tem 2.100 que compõem sua frota de aluguer em Portugal.</p>
-					<p>2003 - A Vale Rent a Car se torna líder nacional em veículos de aluguer graças a uma estratégia baseada em vários franqueados e ao desenvolvimento de várias parcerias com agências de viagens e companhias aéreas.</p>
-					<p>2007 - A Vale Rent a Car é a primeira empresa a fornecer uma frota 100% não-fumador em Portugal.</p>
-					<p>2017 - A Vale Rent a Car renova a sua imagem de marca com um novo slogan, "Moving your way" e um novo logótipo.</p>
+					<p align="justify">2000 - Vale Rent a Car lança um novo serviço de reserva on-line. Este é um grande passo em frente para a empresa. Seu site www.valerentacar.com, tem 2.100 que compõem sua frota de aluguer em Portugal.</p>
+					<p align="justify">2003 - A Vale Rent a Car se torna líder nacional em veículos de aluguer graças a uma estratégia baseada em vários franqueados e ao desenvolvimento de várias parcerias com agências de viagens e companhias aéreas.</p>
+					<p align="justify">2007 - A Vale Rent a Car é a primeira empresa a fornecer uma frota 100% não-fumador em Portugal.</p>
+					<p align="justify">2017 - A Vale Rent a Car renova a sua imagem de marca com um novo slogan, "Moving your way" e um novo logótipo.</p>
 				</div>
 			</div>
 		</div>
@@ -55,8 +55,8 @@
 					<h3 class="panel-title">História</h3>
 				</div>
 				<div class="panel-body">
-					<p>Vale Rent a Car é uma empresa 100% Portuguesa (Madeirense) que opera no mercado Nacional há mais de 20 anos e cuja estrutura acionista privada se mantém até hoje. Fruto da iniciativa e perseverança do seu fundador, José Avelino Rocha, a Vale Rent a Car cresceu, expandiu-se e conta agora com 3 estações na Ilha da Madeira e 10 estações em Portugal Continental, tornando-se numa das principais empresas do sector. Começou com cerca de 25 viaturas num balcão no Funchal e tem agora uma frota de mais de 2.100 viaturas de diferentes segmentos a nível nacional. De alguns colaboradores, tem hoje mais de 120 colaboradores especializados nas suas várias competências comercial, administrativa, manutenção e gestão, permitindo-se abranger as distintas funções de aluguer e gestão da sua frota com equipas próprias.</p>
-					<p>A Vale Rent a Car alcançou em 2011 a Certificação do seu sistema de Gestão da Qualidade (Norma NP EN ISSO 9001 – Gestão da Qualidade: 2008), pela APCER. A Política da Qualidade é assumida como o principal impulsionador da satisfação dos Clientes e da sua fidelização. É o eixo orientador dos processos de melhoria constante, essenciais no desempenho comercial, organizacional e económico da empresa.</p>
+					<p align="justify">Vale Rent a Car é uma empresa 100% Portuguesa (Madeirense) que opera no mercado Nacional há mais de 20 anos e cuja estrutura acionista privada se mantém até hoje. Fruto da iniciativa e perseverança do seu fundador, José Avelino Rocha, a Vale Rent a Car cresceu, expandiu-se e conta agora com 3 estações na Ilha da Madeira e 10 estações em Portugal Continental, tornando-se numa das principais empresas do sector. Começou com cerca de 25 viaturas num balcão no Funchal e tem agora uma frota de mais de 2.100 viaturas de diferentes segmentos a nível nacional. De alguns colaboradores, tem hoje mais de 120 colaboradores especializados nas suas várias competências comercial, administrativa, manutenção e gestão, permitindo-se abranger as distintas funções de aluguer e gestão da sua frota com equipas próprias.</p>
+					<p align="justify">A Vale Rent a Car alcançou em 2011 a Certificação do seu sistema de Gestão da Qualidade (Norma NP EN ISSO 9001 – Gestão da Qualidade: 2008), pela APCER. A Política da Qualidade é assumida como o principal impulsionador da satisfação dos Clientes e da sua fidelização. É o eixo orientador dos processos de melhoria constante, essenciais no desempenho comercial, organizacional e económico da empresa.</p>
 				</div>
 			</div>
 		</div>
